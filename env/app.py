cabd21f3ee8512067f8126a542c94c7fda63efe3
lovehero0206 <tjsdk2769@gmail.com>
2018-05-28 11:10:12 +0900

    User table생성

--- env/app.py
+++ env/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hwang:1234:@localhost/mydatabase'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120),unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
-    return "hello flask"
+    return render_template('add_user.html', name=name)
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    return "<h1 style='color: red'>hello flask</h1>"
 
 if __name__ == "__main__":
     app.run()
-        
